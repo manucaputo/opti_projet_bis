37e0f7e72847bc7519f67d78cb0714fbe06da36b
Adrien Dubois <181887@umons.ac.be>
2021-12-10 13:45:12 +0100

    adri

--- main.py
+++ main.py
@@ -56,57 +56,6 @@ print("taille du vec diviseur2",len(diviseur))
 print("somme du vec diviseur2",sum(diviseur))
 somme=0
 
-
-
-
-def voisinageDivision(vec, iter, nbr,x_list,E):
-    bestprix=float('inf')
-    listDesValeurs=[]
-    for j in range(nbr):
-        list = []
-        if (j==0):
-            best=vec[:]
-
-        for i in range(iter):
-            tmp=best[:]
-            a= randint(0,len(vec)-1)
-            b = randint(0,len(vec)-1)
-            if (tmp[a]>1):
-                tmp[a]=tmp[a]-1
-                tmp[b]=tmp[b]+1
-
-            c = randint(0,len(vec)-1)
-            d = randint(0, len(vec) - 1)
-            val = tmp[c]
-            tmp[c]=tmp[d]
-            tmp[d] = val
-            list.append(tmp)
-
-
-        for d in range(len(list)):
-            prix,listDesValeurs= cost(x_list,list[d],E)
-            if (prix<=bestprix):
-                bestprix=prix
-                best=list[d]
-                bestListDesValeurs= listDesValeurs
-            else:
-                diff= bestprix - prix
-                P= exp(diff)
-                x=random()
-                #print("dif",diff,"p",P,"x",x)
-                if(x<=P):
-                    bestprix=prix
-    print("meilleur cout",bestprix)
-    print(best)
-    print(bestListDesValeurs)
-    print(len(bestListDesValeurs),sum(bestListDesValeurs))
-
-
-    return best
-
-
-
-
 def cost(listvaleur,listdivisuer,E ):
     list=[]
     somme2=0
@@ -142,11 +91,99 @@ def cost(listvaleur,listdivisuer,E ):
 
     return (somme2,list2)
 
+T,listT=cost(x_list,diviseur,E)
+print("valeur de T",T)
+
+def voisinageDivision(vec, iter, nbr,x_list,E,T):
+    bestprix=float('inf')
+    listDesValeurs=[]
+    for j in range(nbr):
+        list = []
+        if (j==0):
+            best=vec[:]
+
+        for i in range(iter):
+            tmp=best[:]
+            a= randint(0,len(vec)-1)
+            b = randint(0,len(vec)-1)
+            if (tmp[a]>1):
+                tmp[a]=tmp[a]-1
+                tmp[b]=tmp[b]+1
+
+            c = randint(0,len(vec)-1)
+            d = randint(0, len(vec) - 1)
+            val = tmp[c]
+            tmp[c]=tmp[d]
+            tmp[d] = val
+            list.append(tmp)
+
+
+        for d in range(len(list)):
+            prix,listDesValeurs= cost(x_list,list[d],E)
+            if (prix<=bestprix):
+                bestprix=prix
+                best=list[d]
+                bestListDesValeurs= listDesValeurs
+            """
+            else:
+                diff= bestprix - prix
+                P= exp(diff/(T/100))
+                x=random()
+               # print("dif",diff,"p",P,"x",x)
+                #if(x<=P):
+                #    bestprix=prix
+            """
+    print("meilleur cout",bestprix)
+    print(best)
+    print(bestListDesValeurs)
+    print(len(bestListDesValeurs),sum(bestListDesValeurs))
+
+
+    return best
+
+
+
+"""
+def cost(listvaleur,listdivisuer,E ):
+    list=[]
+    somme2=0
+
+
+    for i in range(len(listvaleur)):
+        dec=0
+        for j in range(listdivisuer[i]):
+            if (listdivisuer[i]==1):
+                list.append(listvaleur[i])
+            else:
+                dec+=listvaleur[i]/listdivisuer[i]-int(listvaleur[i]/listdivisuer[i])
+                if(j==listdivisuer[i]-1):
+                    list.append(int(listvaleur[i]/listdivisuer[i])+round(dec))
+                else :
+                    list.append(int(listvaleur[i] / listdivisuer[i]))
+
+
+
+    list2=sorted(list,reverse=True)
 
 
 
 
-listtest = voisinageDivision(diviseur,10,1000,x_list,E)
+    for i in range(len(list2)):
+        if(i%E==0):
+
+            somme2+=list2[i]
+
+
+
+    #somme = list2[0]+list2[10]+list2[20]
+
+    return (somme2,list2)
+
+"""
+
+
+
+listtest = voisinageDivision(diviseur,50,1000,x_list,E,T)
 
 print(listtest)
 
