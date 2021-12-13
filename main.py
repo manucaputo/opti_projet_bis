b443c059e6346429feca7064d04ae671a31bb73a
Adrien Dubois <181887@umons.ac.be>
2021-12-13 11:19:23 +0100

    Nouveau vecteur de division

--- main.py
+++ main.py
@@ -4,7 +4,7 @@
 from random import randint, random
 from math import exp
 
-with open("data1.dat", "r") as f:
+with open("data4.dat", "r") as f:
 # Lecture du fichier
     x = []
     N = int(f.readline())
@@ -14,16 +14,17 @@ with open("data1.dat", "r") as f:
     for line in f:
         x.extend(line.strip().split('\t'))
 
-
 x_bis = map(int, x)
 x_list=list(x_bis)
-print(len(x_list))
-print(sum(x_list))
-vec = [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
-#vec = [2, 2, 2, 2, 2, 2, 2, 2, 2, 2,2,2,2,2,2]
+print("nombre d'élément à diviser",len(x_list))
+print("somme de tout les éléments",sum(x_list))
+nbrmoyen=int((sum(x_list)/(B*E)))
+print("division moyenne ",nbrmoyen)
+
 print(B*E/N)
 nbrdiv = int(B*E/N)
 
+#création du vecteur de diviseur
 diviseur=[]
 somdiviseur=B*E
 print("nombre de divison nécessaire",somdiviseur)
@@ -42,24 +43,54 @@ if (nbrdiv==1):
 else:
     for i in range(N):
         diviseur.append(nbrdiv)
-print(diviseur)
-print("taille du vec divisuer",len(diviseur))
-print("somme du vec diviseur",sum(diviseur))
 
+#test d'un deuxiéme diviseur
+diviseur2=[]
+x_list_copie= x_list[:]
+for i in range(len(x_list_copie)):
+    div=1
+    while True:
+        if ((x_list_copie[i]-(2*nbrmoyen))>=0):
+            x_list_copie[i]=x_list_copie[i]-nbrmoyen
+            div+=1
+        else:
+            diviseur2.append(div)
+            break
+
+
+
+
+#si la somme du vecteur de diviseur n'est pas bonne
 if(sum(diviseur)<(B*E)):
     for i in range(len(diviseur)):
         diviseur[i]+=1
         if(sum(diviseur)==(B*E)):
             break
-print(diviseur)
-print("taille du vec diviseur2",len(diviseur))
-print("somme du vec diviseur2",sum(diviseur))
+if(sum(diviseur2)<(B*E)):
+    for i in range(len(diviseur2)):
+        diviseur2[i]+=1
+        if(sum(diviseur2)==(B*E)):
+            break
+if(sum(diviseur2)>(B*E)):
+    for i in range(len(diviseur2)):
+        if(diviseur2[i]>1):
+            diviseur2[i]-=1
+        if(sum(diviseur2)==(B*E)):
+            break
+print("vec diviseur ",diviseur)
+
+print("taille du vec divisuer",len(diviseur))
+print("somme du vec diviseur",sum(diviseur))
+
+print("vec diviseur 2 ",diviseur2)
+print("taille du vec diviseur2",len(diviseur2))
+print("somme du vec diviseur2",sum(diviseur2))
 somme=0
 
+#fonction pour calculer le cout
 def cost(listvaleur,listdivisuer,E ):
     list=[]
     somme2=0
-
 
     for i in range(len(listvaleur)):
         dec=0
@@ -73,28 +104,22 @@ def cost(listvaleur,listdivisuer,E ):
                 else :
                     list.append(int(listvaleur[i] / listdivisuer[i]))
 
-
-
     list2=sorted(list,reverse=True)
-
-
-
 
     for i in range(len(list2)):
         if(i%E==0):
 
             somme2+=list2[i]
-
-
-
-    #somme = list2[0]+list2[10]+list2[20]
-
     return (somme2,list2)
 
+#calcul pour un paramétre du recuit simulé
 T,listT=cost(x_list,diviseur,E)
 print("valeur de T",T)
 
-def voisinageDivision(vec, iter, nbr,x_list,E,T):
+
+# fonction pour créer le voisinage autour du vecteur de divion
+# et recherche local
+def voisinageDivision(vec, iter, nbr,x_list,E,T,T2):
     bestprix=float('inf')
     listDesValeurs=[]
     for j in range(nbr):
@@ -102,6 +127,7 @@ def voisinageDivision(vec, iter, nbr,x_list,E,T):
         if (j==0):
             best=vec[:]
 
+        # création du voisinage
         for i in range(iter):
             tmp=best[:]
             a= randint(0,len(vec)-1)
@@ -117,7 +143,59 @@ def voisinageDivision(vec, iter, nbr,x_list,E,T):
             tmp[d] = val
             list.append(tmp)
 
+        # pour prendre le meilleur dans le voisinage
+        for d in range(len(list)):
+            prix,listDesValeurs= cost(x_list,list[d],E)
+            if (prix<=bestprix):
+                bestprix=prix
+                best=list[d]
+                bestListDesValeurs= listDesValeurs
+            """  
+            else:
+                diff= bestprix - prix
+                P= exp(diff/(T/10))
+                x=random()
+                #print("dif",diff,"p",P,"x",x)
+                if(x<=P):
+                    bestprix=prix
+                if (T>nbrmoyen):
+                    T=T*0.9
+            """
 
+
+    print("meilleur cout",bestprix)
+    print(best)
+    print(bestListDesValeurs)
+    print(len(bestListDesValeurs),sum(bestListDesValeurs))
+
+
+
+    return best
+
+def voisinageDivision2(vec, iter, nbr,x_list,E,T):
+    bestprix=float('inf')
+    listDesValeurs=[]
+    for j in range(nbr):
+        list = []
+        if (j==0):
+            best=vec[:]
+        # création du voisinage
+        for i in range(iter):
+            tmp=best[:]
+
+            a = randint(0, len(vec) - 1)
+            b = randint(0, len(vec) - 1)
+            if (tmp[a] > 1):
+                tmp[a] = tmp[a] - 1
+                tmp[b] = tmp[b] + 1
+
+
+            val = tmp[i]
+            tmp[i]=tmp[i+1]
+            tmp[i+1] = val
+            list.append(tmp)
+
+        # pour prendre le meilleur dans le voisinage
         for d in range(len(list)):
             prix,listDesValeurs= cost(x_list,list[d],E)
             if (prix<=bestprix):
@@ -143,48 +221,9 @@ def voisinageDivision(vec, iter, nbr,x_list,E,T):
 
 
 
-"""
-def cost(listvaleur,listdivisuer,E ):
-    list=[]
-    somme2=0
+#listtest = voisinageDivision2(diviseur,len(diviseur)-1,2000,x_list,E,T)
+listtest = voisinageDivision(diviseur2,30,1000,x_list,E,T,T)
 
-
-    for i in range(len(listvaleur)):
-        dec=0
-        for j in range(listdivisuer[i]):
-            if (listdivisuer[i]==1):
-                list.append(listvaleur[i])
-            else:
-                dec+=listvaleur[i]/listdivisuer[i]-int(listvaleur[i]/listdivisuer[i])
-                if(j==listdivisuer[i]-1):
-                    list.append(int(listvaleur[i]/listdivisuer[i])+round(dec))
-                else :
-                    list.append(int(listvaleur[i] / listdivisuer[i]))
-
-
-
-    list2=sorted(list,reverse=True)
-
-
-
-
-    for i in range(len(list2)):
-        if(i%E==0):
-
-            somme2+=list2[i]
-
-
-
-    #somme = list2[0]+list2[10]+list2[20]
-
-    return (somme2,list2)
-
-"""
-
-
-
-listtest = voisinageDivision(diviseur,50,1000,x_list,E,T)
-
-print(listtest)
+#print(listtest)
 
 
